e14e672f8d4e4abc71fb76cfae45f62b95eaaf1e
silveraMatu <matusilvera808@gmail.com>
2026-09-12 19:04:18 -0300

    Se completo isp

--- ejercicios/04-isp.ts
+++ ejercicios/04-isp.ts
@@ -1,20 +1,19 @@
-interface MultifunctionPrinter {
-  print(document: string): void;
-  scan(document: string): void;
-  fax(document: string): void;
+interface Printer{
+  print(document: string): void
 }
 
-class SimplePrinter implements MultifunctionPrinter {
+interface Scanner{
+  scan(document: string): void
+}
+
+interface Fax{
+  fax(document: string): void
+}
+
+
+class SimplePrinter implements Printer {
   print(document: string): void {
     console.log(`Imprimiendo: ${document}`);
-  }
-
-  scan(_document: string): void {
-    throw new Error("Esta impresora no puede escanear");
-  }
-
-  fax(_document: string): void {
-    throw new Error("Esta impresora no puede enviar fax");
   }
 }
 
